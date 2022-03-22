a8c4c786486ce21e03ad402e71f4496364c1ea68
Fish <icyyfish@gmail.com>
2022-03-22 14:52:08 +0800

    convert to vuepress2

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,44 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: "en-US",
+  title: "Rust 程序设计语言",
+  description: "Translation of The Rust Programming Language",
+
+  // theme and its config
+  theme: "@vuepress/theme-default",
+  themeConfig: {
+    // logo: "https://vuejs.org/images/logo.png",
+    sidebar: [
+      {
+        text: "4. 理解所有权",
+        link: "/ch04/00-understanding-ownership.md",
+        children: [
+          {
+            text: "4.1 什么是所有权",
+            link: "/ch04/01-what-is-ownership.md"
+          },
+          {
+            text: "4.2 引用与借用",
+            link: "/ch04/02-references-and-borrowing.md"
+          },
+          {
+            text: "4.3 Slice 类型",
+            link: "/ch04/03-slices.md"
+          }
+        ]
+      },
+      {
+        text: "5. 使用 Structs 来组织相关联的数据",
+        link: "/ch05/00-structs.md",
+        children: [
+          {
+            text: "5.1 定义并实例化结构体",
+            link: "/ch05/01-defining-structs.md"
+          }
+        ]
+      }
+    ]
+  }
+});
